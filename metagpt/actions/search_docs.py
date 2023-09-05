15000b24707906008b735c1e1376e73ba0016d54
shenchucheng <chuchengshen@fuzhi.ai>
2023-09-05 22:14:20 +0800

    fetcg by aiohttp

--- metagpt/actions/search_docs.py
+++ metagpt/actions/search_docs.py
@@ -1,39 +1,35 @@
-import re
-from typing import List
-
-import requests
+import aiohttp
 from pydantic import BaseModel
 
-from metagpt.logs import logger
 from metagpt.actions.action import Action
+from metagpt.logs import logger
 
-# URL = "http://192.168.50.134:8511/infer" # titan_gpu4
-URL = "http://172.16.32.123:8511/infer" # ucloud-v100s-02
+URL = "http://192.168.50.134:8511/infer"  # titan_gpu4
+# URL = "http://172.16.32.123:8511/infer"  # ucloud-v100s-02
+
 
 class Doc(BaseModel):
     item_id: str = ""
     text: str = ""
     score: float = 0.0
 
+
 class DocResult(BaseModel):
-    docs: List[Doc] = []
+    docs: list[Doc] = []
+
 
 class SearchDocs(Action):
     def __init__(self, name="SearchDocs", context=None, llm=None):
-        super().__init__(name, context, llm)        
-    
-    async def run(self, question, topk=5) -> List[DocResult]:
+        super().__init__(name, context, llm)
 
+    async def run(self, question, topk=5) -> list[DocResult]:
         logger.info(f"search for {question}")
 
-        resp = requests.post(URL, json={"text": question, "topk": topk})
+        async with aiohttp.ClientSession() as client:
+            async with client.post(URL, json={"text": question, "topk": topk}) as resp:
+                if resp.status != 200:
+                    raise RuntimeError(f"Unexpected status code: {resp.status_code}")
 
-        assert resp.status_code == 200, f"Unexpected status code: {resp.status_code}"
-
-        result = resp.json()
-
+        result = await resp.json()
         result = DocResult(docs=[Doc(**item) for item in result["items"]])
-
-        print(*result.docs, sep="\n")
-        
         return result
--- metagpt/roles/company_consultant.py
+++ metagpt/roles/company_consultant.py
@@ -1,9 +1,6 @@
-import asyncio
-
 from metagpt.actions.query_understand import QueryUnderstand
 from metagpt.actions.search_docs import SearchDocs
 from metagpt.actions.summarize_docs import SummarizeDocs
-
 from metagpt.logs import logger
 from metagpt.roles import Role
 from metagpt.schema import Message
@@ -28,12 +25,12 @@ class CompanyConsultant(Role):
             # set state to initial before starting or after one cycle
             self._set_state(0)
             return
-        
+
         previous_state = self._rc.state
         if previous_state == 0:
             # previous state is QU, decide next action based on QU result
             qu_result = self._rc.memory.get(k=1)[0]
-            current_state = self._parse_qu_result(qu_result) # can be 1 or 2
+            current_state = self._parse_qu_result(qu_result)  # can be 1 or 2
             self._set_state(current_state)
         elif previous_state == 1:
             # search done, summarize
@@ -42,7 +39,7 @@ class CompanyConsultant(Role):
             # summarize done, a cycle completed, set state back to intital
             self._set_state(0)
             self._rc.todo = None
-    
+
     async def _act(self) -> Message:
         logger.info(f"{self._setting}: ready to {self._rc.todo}")
         todo = self._rc.todo
@@ -52,18 +49,21 @@ class CompanyConsultant(Role):
             self.original_question = msg.content
             rsp = await QueryUnderstand().run(question=self.original_question)
             msg = Message(content=rsp, role=self.profile, cause_by=todo)
-        
+
         elif isinstance(todo, SearchDocs):
-            question = self.original_question # NOTE: or use self.rewritten_query
+            question = self.original_question  # NOTE: or use self.rewritten_query
             rsp = await SearchDocs().run(question=question, topk=3)
             msg = Message(content="", instruct_content=rsp, role=self.profile, cause_by=todo)
-        
+
         elif isinstance(todo, SummarizeDocs):
-            doc_result = msg.instruct_content # might be None (from QueryUnderstand) or a DocResult object (from SearchDocs)
+            doc_result = (
+                msg.instruct_content
+            )  # might be None (from QueryUnderstand) or a DocResult object (from SearchDocs)
             rsp = await SummarizeDocs().run(question=self.original_question, doc_result=doc_result)
             msg = Message(content=rsp, role=self.profile, cause_by=todo)
-        
+
         self._rc.memory.add(msg)
+        return msg
 
     async def _react(self) -> Message:
         while True:
@@ -71,15 +71,16 @@ class CompanyConsultant(Role):
             if self._rc.todo is None:
                 break
             await self._act()
-        return Message(content="One conversation done", role=self.profile) # FIXME
+        return Message(content="One conversation done", role=self.profile)  # FIXME
 
     def _parse_qu_result(self, qu_rsp):
         content = qu_rsp.content
         if "SEARCH" in content:
-            self.rewritten_query = content.split("+")[-1] # hard code splitter for now
+            self.rewritten_query = content.split("+")[-1]  # hard code splitter for now
             return 1
         else:
             return 2
+
 
 if __name__ == "__main__":
     import fire
