fd48278e8c558a609561b993a52077d72fc84929
张建生 <jasonzhang@fuzhi.ai>
2023-12-21 11:02:42 +0800

    update:增加检测失败原因

--- metagpt/tools/moderation.py
+++ metagpt/tools/moderation.py
@@ -13,24 +13,26 @@ class Moderation:
     def __init__(self):
         self.llm = LLMFactory.new_llm()
 
+    def handle_moderation_results(self, results):
+        resp = []
+        for item in results:
+            categories = item.categories
+            true_categories = [category for category, item_flagged in categories.items() if item_flagged]
+            resp.append({"flagged": item.flagged, "true_categories": true_categories})
+        return resp
+
     def moderation(self, content: Union[str, list[str]]):
         resp = []
         if content:
             moderation_results = self.llm.moderation(content=content)
-            results = moderation_results.results
-            for item in results:
-                resp.append(item.flagged)
-
+            resp = self.handle_moderation_results(moderation_results.results)
         return resp
 
     async def amoderation(self, content: Union[str, list[str]]):
         resp = []
         if content:
             moderation_results = await self.llm.amoderation(content=content)
-            results = moderation_results.results
-            for item in results:
-                resp.append(item.flagged)
-
+            resp = self.handle_moderation_results(moderation_results.results)
         return resp
 
 
